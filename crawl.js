b0a869b17973b70d21d370537da78faf44acb474
batursertan <sertan.batur.estgn@gmail.com>
2023-07-04 16:04:55 +0300

    normalizeURL and tests (thumbs up emoji)

--- crawl.js
+++ crawl.js
@@ -1,7 +1,29 @@
-function normalizeURL(url){
+const { JSDOM } = require('jsdom')
 
+function normalizeURL(urlStr){
+  try {  
+  const urlObj = new URL(urlStr)
+  const path = `${urlObj.hostname}${urlObj.pathname}`
+  if (path.length > 0 && path.slice(-1) === '/'){
+    return path.slice(0,-1)
+  }
+  return path
+} 
+catch (error) {
+  return 'please enter a valid URL(http included)'
+    
+}
+}
+module.exports ={
+    normalizeURL
 }
 
+function getURLsFromHTML(htmlBody, baseURL){
+  const dom = new JSDOM(htmlBody)
+  const foundURL = dom.window.document.querySelector("a")
+  
+  return foundURL
+}
 
 
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,22 +1,49 @@
 const { normalizeURL } = require('./crawl.js')
+const { getURLsFromHTML } = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
 
 
-test('yes',() => {
-    expect(normalizeURL('https://wagslane.dev/path/')).toBe('wagslane.dev/path.');
+test('normalize url with / at the end',() => {
+    const input = 'https://wagsLane.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'wagslane.dev/path'
+    expect(actual).toEqual(expected);
 });
 
-test('yes',() => {
-    expect(normalizeURL('https://wagsLane.Dev/path')).toBe('wagslane.dev/path.');
+test('normalize url with CAPITALS',() => {
+    const input = 'https://WAGSLaNe.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'wagslane.dev/path'
+    expect(actual).toEqual(expected);
 });
 
-test('yes',() => {
-    expect(normalizeURL('https://wagslane.dev/path')).toBe('wagslane.dev/path.');
+test('normalize url with http',() => {
+    const input = 'http://wagsLane.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'wagslane.dev/path'
+    expect(actual).toEqual(expected);
 });
 
-test('yes',() => {
-    expect(normalizeURL('http://wagslane.dev/path')).toBe('wagslane.dev/path.');
-});
+ test('normalize url already normalized (**FAILED**)',() => {
+    const input = 'wagsLane.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'wagslane.dev/path'
+    expect(actual).toEqual(expected);
+ }); 
 
-test('yes',() => {
-    expect(normalizeURL('wagslane.dev/path')).toBe('wagslane.dev/path.');
-});
+
+
+
+test('getURLsFromHTML', ()=> {
+    const inputHTML = ' <html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>'
+    const inputURL = ''
+    const actual = getURLsFromHTML(inputHTML, inputURL)
+    const expected = ''
+    expect(actual).toBe(expected)
+})
+
+
+
+
+
+
