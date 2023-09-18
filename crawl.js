9f5238854c6e96f8752eba929dd1c1e3296e60cc
batursertan <sertan.batur.estgn@gmail.com>
2023-09-18 14:06:21 +0300

    recursive crawl page working

--- crawl.js
+++ crawl.js
@@ -27,10 +27,8 @@ function getURLsFromHTML(htmlBody, baseURL){
         const urlObj = new URL(`${baseURL}${linkElement.href}`)
         urls.push(urlObj.href)
       } catch (error) {
-        console.log(`error with relative url: ${error.message}`)
-        
-      }
-      
+        console.log(`error with relative url: ${error.message}`)       
+      }    
     }
     else{
       //absolute
@@ -38,40 +36,64 @@ function getURLsFromHTML(htmlBody, baseURL){
         const urlObj = new URL(linkElement.href)
         urls.push(urlObj.href)
       } catch (error) {
-        console.log(`error with absolute url: ${error.message}`)
-        
+        console.log(`error with absolute url: ${error.message}`)   
       }
-     
-    }
-    
+    }   
   }
   return urls
 }
 
 
-async function crawlPage(current_URL){
-  console.log(`crawling: ${current_URL}`)
+async function crawlPage(baseURL, current_URL, pages){
+ 
+
+  const urlObjBase = new URL(baseURL)
+  const urlObjCurent = new URL(current_URL)
+  if ((urlObjBase.hostname) !== (urlObjCurent.hostname)){
+    return pages 
+  }
+
+  const normalizedCurrent= normalizeURL(current_URL)
+  if (pages[normalizedCurrent] > 0 ){
+    pages[normalizedCurrent]++
+  return pages
+}
+
+if (current_URL === baseURL){
+  pages[normalizedCurrent] = 0
+} else {
+  pages[normalizedCurrent] = 1
+}
 
 
+console.log(`actively crawling: ${current_URL}`)
+ 
   try {
     const resp = await fetch(current_URL)
 
     if (resp.status > 399){
       console.log(`error in fetch with status code: ${resp.status} on page: ${current_URL}`)
-      return
+      return pages
     } 
-
     const contentType = resp.headers.get("content-type")
     if (!contentType.includes("text/html")){
       console.log(`non html response, content type: ${contentType}, on page: ${current_URL}`)
-      return
+      return pages
     }
 
-    console.log(await resp.text())
+    const htmlBody = await resp.text()
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs){
+      pages = await crawlPage(baseURL, nextURL, pages)
+
+    }
+
   } catch (error) {
       console.log(`error in fetch: ${error.message}, on page: ${current_URL}`)
   }
-  
+
+  return pages
 }
 
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl.js')
 
-function main(){
+async function main(){
   if (process.argv.length < 3){
     console.log("please provide a website")
     process.exit(1)
@@ -16,7 +16,10 @@ function main(){
 
   console.log(`starting crawling of ${baseURL}`) 
 
-  crawlPage(baseURL)
+  const pages = await crawlPage(baseURL, baseURL, {})
+  for (const page of Object.entries(pages)){
+    console.log(page)
+  }
   }
   
 
